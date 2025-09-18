4d117a711bbe06c9c1e54cbbc239764a41799ed8
ZSL <shuilinzhang66@gmail.com>
2025-09-18 22:32:58 +0800

    test code

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,10 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("@chainlink/env-enc").config();
 require("./tasks")
+require("hardhat-deploy")
+require("@nomicfoundation/hardhat-ethers");
+require("hardhat-deploy");
+require("hardhat-deploy-ethers");
 
 const SEPOLIA_KRY = process.env.SEPOLIA_KRY;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
@@ -9,6 +13,10 @@ const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
+  defaultNetwork: "hardhat",
+  mocha: {
+    timeout: 300000 // 300 seconds max for running tests
+  },
   networks: {
     sepolia: {
       url: SEPOLIA_KRY, // replace with your Alchemy API key
@@ -20,5 +28,16 @@ module.exports = {
     apiKey:{
       sepolia: ETHERSCAN_API_KEY
     }                     // replace with your Etherscan API key
+  },
+  namedAccounts: {
+    firstAccount: {
+      default: 0, // here this will by default take the first account as deployer
+    },
+    secondAccount: {
+      default: 1
+    },
+  gasReporter: {
+    enabled: true // set to true to enable gas reporting
+  }
   }
 };
